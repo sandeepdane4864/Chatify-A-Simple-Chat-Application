7292b5f49976eb46bae82f06314a6fa6dcc361c9
Sandeep Dane <150518108+sandeepdane4864@users.noreply.github.com>
2026-03-30 23:30:17 +0530

    Add Socket.IO server initialization

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const http = require('http');
 const Chat = require('./models/chat');
 
 // SERVER + SOCKET
+
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
